f45d9105f2db0e55e6ed9e768771effc0d332aa4
baitian <1749492810@qq.com>
2024-05-05 21:41:38 +0800

    1

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -5,8 +5,8 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "文档演示",
-  description: "vuepress-theme-hope 的文档演示",
+  title: "C#学习",
+  description: "关于我的C#学习",
 
   theme,
 
--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -25,6 +25,6 @@ export default navbar([
   {
     text: "V2 文档",
     icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
+    link: "",
   },
 ]);
