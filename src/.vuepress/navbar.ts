9d732c444453fd16793e65d3a5f206fe547c3bad
Gce <1749492810@qq.com>
2024-07-16 13:37:18 +0800

    改变导航栏

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,30 +2,91 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  "/portfolio",
-  "/demo/",
   {
-    text: "指南",
+    text: "C#指南",
     icon: "lightbulb",
-    prefix: "/guide/",
+    prefix: "/c#-basics/",
     children: [
       {
-        text: "Bar",
+        text: "C#",
         icon: "lightbulb",
-        prefix: "bar/",
-        children: ["baz", { text: "...", icon: "ellipsis", link: "" }],
+        prefix: "c#/",
+        children: [ { text: "C#基础", icon: "ellipsis", link: "" }],
       },
       {
-        text: "Foo",
+        text: "数据库",
         icon: "lightbulb",
-        prefix: "foo/",
-        children: ["ray", { text: "...", icon: "ellipsis", link: "" }],
+        prefix: "database/",
+        children: [ { text: "数据库", icon: "ellipsis", link: "" }],
+      },
+      {
+        text: "计算机基础",
+        icon: "lightbulb",
+        prefix: "cs-basics/",
+        children: [{ text: "计算机基础", icon: "ellipsis", link: "" }],
       },
     ],
   },
   {
-    text: "V2 文档",
+    text: "UI界面开发",
+    icon: "lightbulb",
+    prefix: "/guide/",
+    children: [
+      {
+        text: "Winform",
+        icon: "lightbulb",
+        prefix: "winform/",
+        children: [ { text: "Winform", icon: "ellipsis", link: "" }],
+      },
+      {
+        text: "WPF",
+        icon: "lightbulb",
+        prefix: "wpf/",
+        children: [ { text: "WPF", icon: "ellipsis", link: "" }],
+      },
+    ],
+  },
+  {
+    text: "通讯协议",
     icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
+    link: "/communication-protocol/",
+  },
+  {
+    text: "项目实战",
+    icon: "lightbulb",
+    prefix: "/guide/",
+    children: [
+      {
+        text: "开源项目",
+        icon: "lightbulb",
+        prefix: "bar/",
+        children: [ { text: "开源项目", icon: "ellipsis", link: "" }],
+      },
+      {
+        text: "项目实战",
+        icon: "lightbulb",
+        prefix: "foo/",
+        children: [ { text: "项目实战", icon: "ellipsis", link: "" }],
+      },
+    ],
+  },
+  {
+    text: "其它",
+    icon: "lightbulb",
+    prefix: "/other/",
+    children: [
+      {
+        text: "文档编写",
+        icon: "lightbulb",
+        prefix: "document-write/",
+        children: [ { text: "文档编写", icon: "ellipsis", link: "" }],
+      },
+      {
+        text: "关系维护",
+        icon: "lightbulb",
+        prefix: "relationship-maintenance/",
+        children: [ { text: "关系维护", icon: "ellipsis", link: "" }],
+      },
+    ],
   },
 ]);
